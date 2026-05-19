f14d87f4616c9c5316dbfd38fd6f5195d22399cc
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2026-05-19 20:30:15 -0300

    feat: Criação das specifications para verificar documento e email

---
+++ src/GestoDePedidos.Domain/Specifications/DocumentAlreadyExists.cs
@@ -0,0 +1,12 @@
+﻿namespace GestoDePedidos.Domain.Specifications;
+
+public class DocumentAlreadyExists : Specification<Cliente>
+{
+    public DocumentAlreadyExists(string documento)
+    {
+        Query.Where(c =>
+            c.Documento == documento &&
+            c.Ativo
+        );
+    }
+}
---
+++ src/GestoDePedidos.Domain/Specifications/EmailAlreadyExists.cs
@@ -0,0 +1,13 @@
+﻿namespace GestoDePedidos.Domain.Specifications;
+
+public class EmailAlreadyExists : Specification<Cliente>
+{
+    public EmailAlreadyExists(string email)
+    {
+        Query.Where(
+            c => 
+                c.Email == email && 
+                c.Ativo
+        );
+    }
+}

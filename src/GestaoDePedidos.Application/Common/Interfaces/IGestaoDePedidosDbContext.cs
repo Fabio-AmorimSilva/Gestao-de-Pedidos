6984fbfdf230e5e734e49326be4c8ad2cffa7c68
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2026-05-19 20:29:23 -0300

    feat: Criação da interface do banco de dados

---
+++ src/GestaoDePedidos.Application/Common/Interfaces/IGestaoDePedidosDbContext.cs
@@ -0,0 +1,10 @@
+﻿namespace GestaoDePedidos.Application.Common.Interfaces;
+
+public interface IGestaoDePedidosDbContext
+{
+    DbSet<Cliente> Clientes { get; set; }
+    DbSet<Pedido> Pedidos { get; set; }
+    DbSet<Produto> Produtos { get; set; }
+    
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}

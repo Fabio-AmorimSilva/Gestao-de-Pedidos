8c0027adbf48189ea94ffcf8197f5af80927ff10
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2026-05-19 20:31:26 -0300

    feat: Criando mensagens de erro

---
+++ src/GestoDePedidos.Domain/Messages/ErrorMessages.cs
@@ -0,0 +1,22 @@
+﻿namespace GestoDePedidos.Domain.Messages;
+
+public static class ErrorMessages
+{
+    public static string CannotBeEmpty(string field)
+        => $"{field} cannot be empty";
+
+    public static string HasMaxLength(string field, int length)
+        => $"{field} must have a maximum of {length} characters";
+
+    public static string NotFound(string field)
+        => $"{field} cannot be found";
+    
+    public static string NotFound<T>()
+        => $"{typeof(T).Name} cannot be found";
+    
+    public static string MustBeUnique(string field)
+        => $"{field} must be unique";
+
+    public static string InvalidEmail()
+        => $"Email is invalid";
+}

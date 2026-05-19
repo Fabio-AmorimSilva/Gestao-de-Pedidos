9912567c2076994b15e86848982597e83c7ce4e7
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2026-05-19 20:24:29 -0300

    feat: Criando injeção de dependência para camada de application e infrastructure

---
+++ src/GestaoDePedidos.Application/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace GestaoDePedidos.Application;
+
+public static class DependencyInjection
+{
+    extension(IServiceCollection services)
+    {
+        public IServiceCollection AddApplication()
+        {
+            services.AddServices();
+            
+            return services;
+        }
+
+        private IServiceCollection AddServices()
+        {
+            services.AddScoped<IClienteService, ClienteService>();
+            services.AddScoped<IProdutoService, ProdutoService>();
+            
+            
+            return services;
+        }
+    }
+}
---
+++ src/GestaoDePedidos.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,30 @@
+﻿using GestaoDePedidos.Application.Common.Interfaces;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class DependencyInjection
+{
+    extension(IServiceCollection services)
+    {
+        public IServiceCollection AddInfrastructure(IConfiguration configuration)
+        {
+            services
+                .AddDbContextAndInterceptors(configuration);
+            
+            return services;
+        }
+
+        private IServiceCollection AddDbContextAndInterceptors(IConfiguration configuration)
+        {
+            services.AddDbContext<GestaoDePedidosDbContext>((sp, options) =>
+            {
+                options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddScoped<IGestaoDePedidosDbContext>(provider => provider.GetRequiredService<GestaoDePedidosDbContext>());
+            
+            return services;
+        }
+    }
+}

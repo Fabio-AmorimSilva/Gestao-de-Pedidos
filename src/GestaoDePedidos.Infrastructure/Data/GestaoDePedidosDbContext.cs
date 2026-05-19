28903b9440fb8167a667a9e1319f068383a1a8c6
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2026-05-19 20:25:14 -0300

    feat: Criando GestaoDePedidosDbContext

---
+++ src/GestaoDePedidos.Infrastructure/Data/GestaoDePedidosDbContext.cs
@@ -0,0 +1,18 @@
+﻿using GestaoDePedidos.Application.Common.Interfaces;
+
+namespace GestaoDePedidos.Infrastructure.Data;
+
+public class GestaoDePedidosDbContext(DbContextOptions<GestaoDePedidosDbContext> options) : 
+    DbContext(options),
+    IGestaoDePedidosDbContext
+{
+    public DbSet<Cliente> Clientes { get; set; }
+    public DbSet<Pedido> Pedidos { get; set; }
+    public DbSet<Produto> Produtos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(modelBuilder);
+    }
+}

4654c40fbf48af06116abe9d43a2a2f629e87d77
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2026-05-19 20:13:54 -0300

    feat: Criação da interface de auditoria IAuditableEntity

---
+++ src/GestoDePedidos.Domain/Common/IAuditableEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace GestoDePedidos.Domain.Common;
+
+public interface IAuditableEntity
+{
+    public DateTime DataCriacao { get; set; }
+    public DateTime? DataAtualizacao { get; set; }
+}

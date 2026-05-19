383b88fa875599e4901ef2e1769f4113a1909cd7
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2026-05-19 20:13:08 -0300

    feat: Criação da entidade cliente

---
+++ src/GestoDePedidos.Domain/Entities/Cliente.cs
@@ -0,0 +1,34 @@
+﻿namespace GestoDePedidos.Domain.Entities;
+
+public class Cliente : Entity, IAuditableEntity
+{
+    public const int NomeMaxLength = 200; 
+    
+    public string Nome { get; private set; }
+    public string Email { get; private set; }
+    public string Documento { get; private set; }
+    public bool Ativo { get; private set; }
+    public DateTime DataCriacao { get; set; }
+    public DateTime? DataAtualizacao { get; set; }
+
+    public Cliente(
+        string nome,
+        string email,
+        string documento
+    )
+    {
+        Guard.IsNotEmpty(nome);
+        Guard.IsNotEmpty(email);
+        Guard.IsNotEmpty(documento);
+        
+        Nome = nome;
+        Email = email;
+        Documento = documento;
+        Ativo = true;
+    }
+
+    public void Desativar()
+    {
+        Ativo = false;
+    }
+}

def083bce55b80fe0b1b40f74d97ba1177a7ab12
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2026-05-19 20:26:12 -0300

    feat: Criando configuração de banco para entidade cliente

---
+++ src/GestaoDePedidos.Infrastructure/Configurations/ClienteEntityTypeConfiguration.cs
@@ -0,0 +1,41 @@
+﻿namespace GestaoDePedidos.Infrastructure.Configurations;
+
+public class ClienteEntityTypeConfiguration : IEntityTypeConfiguration<Cliente>
+{
+    public void Configure(EntityTypeBuilder<Cliente> builder)
+    {
+        builder
+            .ToTable("Clientes");
+        
+        builder
+            .HasKey(c => c.Id);
+
+        builder
+            .HasIndex(c => c.Documento);
+
+        builder
+            .Property(c => c.Nome)
+            .HasMaxLength(Cliente.NomeMaxLength)
+            .IsRequired();
+
+        builder
+            .Property(c => c.Email)
+            .IsRequired();
+        
+        builder
+            .Property(c => c.Documento)
+            .IsRequired();
+        
+        builder
+            .Property(c => c.Ativo)
+            .IsRequired();
+        
+        builder
+            .Property(c => c.DataCriacao)
+            .IsRequired();
+        
+        builder
+            .Property(c => c.DataAtualizacao)
+            .IsRequired(false);
+    }
+}

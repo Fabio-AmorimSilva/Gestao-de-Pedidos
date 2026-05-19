835cc3f95949e348ec50c594a56fe9811d3fae96
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2026-05-19 20:39:26 -0300

    refactor: Adicionando null! para reduzir warnings no código

---
+++ src/GestaoDePedidos.Application/Services/Clientes/Dtos/ClienteResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace GestaoDePedidos.Application.Services.Clientes.Dtos;
+
+public record ClienteResponseDto
+{
+    public string Nome { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Documento { get; set; } = null!;
+}

554c3b3588b52819750a2e4cae498b2d9c325172
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2026-05-20 09:58:46 -0300

    feat: Criação da entidade PedidoHistoricoStatus

---
+++ src/GestoDePedidos.Domain/Entities/PedidoHistoricoStatus.cs
@@ -0,0 +1,25 @@
+﻿namespace GestoDePedidos.Domain.Entities;
+
+public class PedidoHistoricoStatus : Entity
+{
+    public Guid PedidoId { get; private set; }
+    public Pedido Pedido { get; private set; }
+    public StatusPedido StatusAnterior { get; private set; }
+    public StatusPedido StatusPosterior { get; private set; }
+    public DateTime DataAlteracao { get; private set; }
+    public string? Motivo { get; private set; }
+
+    public PedidoHistoricoStatus(
+        Guid pedidoId,
+        StatusPedido statusAnterior,
+        StatusPedido statusPosterior,
+        string? motivo
+    )
+    {
+        PedidoId = pedidoId;
+        StatusAnterior = statusAnterior;
+        StatusPosterior = statusPosterior;
+        DataAlteracao = DateTime.UtcNow;
+        Motivo = motivo;
+    }
+}

cbe5cfb267cea49dc0fb19cedd03d7b9ff21d4d9
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2026-05-20 09:22:04 -0300

    feat: Criando AlterarStatusPedidoPagoUseCase

---
+++ src/GestaoDePedidos.Application/UseCases/Pedidos/AlterarStatusPedidoParaPago/AlterarStatusPedidoPagoUseCase.cs
@@ -0,0 +1,27 @@
+﻿namespace GestaoDePedidos.Application.UseCases.Pedidos.AlterarStatusPedidoParaPago;
+
+public class AlterarStatusPedidoPagoUseCase(IGestaoDePedidosDbContext context) : IAlterarStatusPedidoPagoUseCase
+{
+    public async Task<Response> ExecuteAsync(AlterarStatusDto dto)
+    {
+        var pedido = await context.Pedidos.FirstOrDefaultAsync(p => p.Id == dto.PedidoId);
+        
+        if(pedido is null)
+            return new NotFoundResponse<Pedido>(ErrorMessages.NotFound<Pedido>());
+        
+        var statusAnterior = pedido.Status;
+        pedido.StatusPago();
+        
+        var historico = new PedidoHistoricoStatus(
+            pedidoId: pedido.Id,
+            statusAnterior: statusAnterior,
+            statusPosterior: pedido.Status,
+            motivo: dto.Motivo
+        );
+
+        await context.PedidoHistoricoStatus.AddAsync(historico);
+        await context.SaveChangesAsync();
+
+        return new NoContentResponse();
+    }
+}
---
+++ src/GestaoDePedidos.Application/UseCases/Pedidos/AlterarStatusPedidoParaPago/IAlterarStatusPedidoPagoUseCase.cs
@@ -0,0 +1,6 @@
+﻿namespace GestaoDePedidos.Application.UseCases.Pedidos.AlterarStatusPedidoParaPago;
+
+public interface IAlterarStatusPedidoPagoUseCase
+{
+    Task<Response> ExecuteAsync(AlterarStatusDto dto);
+}

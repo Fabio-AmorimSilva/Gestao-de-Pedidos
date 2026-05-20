f6aa6d3b7b87630ab4517b74c72620b6f6736c2c
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2026-05-19 23:22:01 -0300

    feat: Adicionando UseCases na injeção de dependência da aplicação

--- src/GestaoDePedidos.Application/DependencyInjection.cs
+++ src/GestaoDePedidos.Application/DependencyInjection.cs
@@ -7,17 +7,26 @@ public static class DependencyInjection
         public IServiceCollection AddApplication()
         {
             services
-                .AddServices()
+                .AddUseCases()
                 .AddValidators();
             
             return services;
         }
 
-        private IServiceCollection AddServices()
+        private IServiceCollection AddUseCases()
         {
-            services.AddScoped<IClienteService, ClienteService>();
-            services.AddScoped<IProdutoService, ProdutoService>();
+            services.AddScoped<ICriarClienteUseCase, CriarClienteUseCase>();
+            services.AddScoped<IObterClientePorIdUseCase, ObterClientePorIdUseCase>();
+            services.AddScoped<IObterClientesUseCase, ObterClientesUseCase>();
+            services.AddScoped<IDesativarClienteUseCase, DesativarClienteUseCase>();
             
+            services.AddScoped<IAtivarProdutoUseCase, AtivarProdutoUseCase>();
+            services.AddScoped<IAtualizarEstoqueUseCase, AtualizarEstoqueUseCase>();
+            services.AddScoped<IAtualizarProdutoUseCase, AtualizarProdutoUseCase>();
+            services.AddScoped<ICadastrarProdutoUseCase, CadastrarProdutoUseCase>();
+            services.AddScoped<IDesativarProdutoUseCase, DesativarProdutoUseCase>();
+            services.AddScoped<IObterProdutoPorIdUseCase, ObterProdutoPorIdUseCase>();
+            services.AddScoped<IObterProdutosUseCase, ObterProdutosUseCase>();
             
             return services;
         }

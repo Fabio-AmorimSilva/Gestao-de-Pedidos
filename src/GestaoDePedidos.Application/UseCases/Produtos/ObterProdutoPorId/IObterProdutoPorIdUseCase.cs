f5626e88a37b4ba4cdbf098d98efe3b13c55f5e3
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2026-05-19 23:25:17 -0300

    feat: Criando ObterProdutoPorIdUseCase e classes relacionadas

---
+++ src/GestaoDePedidos.Application/UseCases/Produtos/ObterProdutoPorId/IObterProdutoPorIdUseCase.cs
@@ -0,0 +1,6 @@
+﻿namespace GestaoDePedidos.Application.UseCases.Produtos.ObterProdutoPorId;
+
+public interface IObterProdutoPorIdUseCase
+{
+    Task<Response<ObterProdutoPorIdUseCaseModel>> ExecuteAsync(Guid id);
+}
---
+++ src/GestaoDePedidos.Application/UseCases/Produtos/ObterProdutoPorId/ObterProdutoPorIdUseCase.cs
@@ -0,0 +1,26 @@
+﻿namespace GestaoDePedidos.Application.UseCases.Produtos.ObterProdutoPorId;
+
+public class ObterProdutoPorIdUseCase(IGestaoDePedidosDbContext context) : IObterProdutoPorIdUseCase
+{
+    public async Task<Response<ObterProdutoPorIdUseCaseModel>> ExecuteAsync(Guid id)
+    {
+        var produto = await context.Produtos
+            .AsNoTracking()
+            .FirstOrDefaultAsync(p => p.Id == id);
+
+        if (produto is null)
+            return new NotFoundResponse<ObterProdutoPorIdUseCaseModel>(ErrorMessages.NotFound<Produto>());
+
+        return new OkResponse<ObterProdutoPorIdUseCaseModel>(new ObterProdutoPorIdUseCaseModel
+        {
+            Id = produto.Id,
+            Nome = produto.Nome,
+            Descricao = produto.Descricao,
+            Preco = produto.Preco,
+            Estoque = produto.Estoque,
+            Ativo = produto.Ativo,
+            DataCriacao = produto.DataCriacao,
+            DataAtualizacao = produto.DataAtualizacao
+        });
+    }
+}

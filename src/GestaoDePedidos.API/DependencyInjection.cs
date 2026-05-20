12b7b5a5b39b8b57dc933b1bf67f1839e3d10e16
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2026-05-19 23:31:45 -0300

    feat: Criando DependencyInjection para API

---
+++ src/GestaoDePedidos.API/DependencyInjection.cs
@@ -0,0 +1,34 @@
+﻿namespace Microsoft.Extensions.DependencyInjection;
+
+public static class DependencyInjection
+{
+    extension(IServiceCollection services)
+    {
+        public IServiceCollection AddApi()
+        {
+            services
+                .AddGlobalExceptionHandler()
+                .AddProblemDetailsService();
+        
+            return services;
+        }
+
+        private void AddProblemDetailsService()
+        {
+            services.AddProblemDetails(configure =>
+            {
+                configure.CustomizeProblemDetails = context =>
+                {
+                    context.ProblemDetails.Extensions.TryAdd("requestId", context.HttpContext.TraceIdentifier);
+                };
+            });
+        }
+
+        private IServiceCollection AddGlobalExceptionHandler()
+        {
+            services.AddExceptionHandler<GlobalExceptionHandler>();
+        
+            return services;
+        }
+    }
+}

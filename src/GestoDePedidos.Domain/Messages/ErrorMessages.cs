c19436236355bf639017efac38db1ac391f728c2
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2026-05-20 20:15:42 -0300

    refactor: Atualizando nome dos parâmetros dos métodos para português

--- src/GestoDePedidos.Domain/Messages/ErrorMessages.cs
+++ src/GestoDePedidos.Domain/Messages/ErrorMessages.cs
@@ -2,20 +2,23 @@
 
 public static class ErrorMessages
 {
-    public static string NaoPodeSerVazio(string field)
-        => $"{field} não pode ser vazio";
+    public static string NaoPodeSerVazio(string campo)
+        => $"{campo} não pode ser vazio";
 
-    public static string TemTamanhoMaximo(string field, int length)
-        => $"{field} deve ter o tamanho máximo de {length} caracteres";
+    public static string TemTamanhoMaximo(string campo, int tamanho)
+        => $"{campo} deve ter o tamanho máximo de {tamanho} caracteres";
+    
+    public static string DeveSerMaiorQue(string campo, int valor)
+        => $"{campo} deve mairo que {valor}";
 
-    public static string NaoEncontrado(string field)
-        => $"{field} não pode ser encontrado";
+    public static string NaoEncontrado(string campo)
+        => $"{campo} não pode ser encontrado";
     
     public static string NaoEncontrado<T>()
         => $"{typeof(T).Name} não pode ser encontrado";
     
-    public static string DeveSerUnico(string field)
-        => $"{field} deve ser único";
+    public static string DeveSerUnico(string campo)
+        => $"{campo} deve ser único";
 
     public static string EmailInvalido()
         => "O Email é inválido";

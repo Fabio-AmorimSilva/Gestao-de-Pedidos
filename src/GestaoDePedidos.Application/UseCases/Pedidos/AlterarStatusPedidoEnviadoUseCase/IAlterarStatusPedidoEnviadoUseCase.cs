c7840326a58c2703c2bf7852524320c38771ec93
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2026-05-20 09:22:36 -0300

    feat: Criando AlterarStatusPedidoEnviadoUseCase

---
+++ src/GestaoDePedidos.Application/UseCases/Pedidos/AlterarStatusPedidoEnviadoUseCase/IAlterarStatusPedidoEnviadoUseCase.cs
@@ -0,0 +1,6 @@
+﻿namespace GestaoDePedidos.Application.UseCases.Pedidos.AlterarStatusPedidoEnviadoUseCase;
+
+public interface IAlterarStatusPedidoEnviadoUseCase
+{
+    Task<Response> ExecuteAsync(AlterarStatusDto dto);
+}

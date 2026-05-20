2f5a268ce505908bde3b55a00cfb09c5bb8c7f07
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2026-05-20 09:22:19 -0300

    feat: Criando AlterarStatusPedidoParaCanceladoUseCase

---
+++ src/GestaoDePedidos.Application/UseCases/Pedidos/AlterarStatusPedidoParaCanceladoUseCase/AlterarStatusPedidoParaCanceladoUseCase.cs
@@ -0,0 +1,28 @@
+﻿namespace GestaoDePedidos.Application.UseCases.Pedidos.AlterarStatusPedidoParaCanceladoUseCase;
+
+public class AlterarStatusPedidoParaCanceladoUseCase(IGestaoDePedidosDbContext context) : IAlterarStatusPedidoParaCanceladoUseCase
+{
+    public async Task<Response> ExecuteAsync(AlterarStatusDto dto)
+    {
+        var pedido = await context.Pedidos.FirstOrDefaultAsync(p => p.Id == dto.PedidoId);
+        
+        if(pedido is null)
+            return new NotFoundResponse<Pedido>(ErrorMessages.NotFound<Pedido>());
+        
+        var statusAnterior = pedido.Status;
+        pedido.StatusCancelado();
+
+        var historico = new PedidoHistoricoStatus(
+            pedidoId: pedido.Id,
+            statusAnterior: statusAnterior,
+            statusPosterior: pedido.Status,
+            motivo: dto.Motivo
+        );
+        
+        await context.PedidoHistoricoStatus.AddAsync(historico);
+        
+        await context.SaveChangesAsync();
+
+        return new NoContentResponse();
+    }
+}
---
+++ src/GestaoDePedidos.Application/UseCases/Pedidos/AlterarStatusPedidoParaCanceladoUseCase/IAlterarStatusPedidoParaCanceladoUseCase.cs
@@ -0,0 +1,6 @@
+﻿namespace GestaoDePedidos.Application.UseCases.Pedidos.AlterarStatusPedidoParaCanceladoUseCase;
+
+public interface IAlterarStatusPedidoParaCanceladoUseCase
+{
+    Task<Response> ExecuteAsync(AlterarStatusDto dto);
+}

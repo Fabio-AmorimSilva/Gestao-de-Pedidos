bd23fc31cad429ffba1037f33b7f15e9a25ead25
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2026-05-20 09:23:00 -0300

    feat: Criando AlterarStatusDto para use cases de status

---
+++ src/GestaoDePedidos.Application/UseCases/Pedidos/Dtos/AlterarStatusDto.cs
@@ -0,0 +1,7 @@
+﻿namespace GestaoDePedidos.Application.UseCases.Pedidos.Dtos;
+
+public record AlterarStatusDto
+{
+    public Guid PedidoId { get; init; }
+    public string? Motivo { get; set; }
+}

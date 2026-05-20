53a44a8fed1c83d9189144789047dc2cfb55972e
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2026-05-19 23:29:28 -0300

    feat: Criar ObterClientePorIdUseCase e classes relacionadas

---
+++ src/GestaoDePedidos.Application/UseCases/Clientes/ObterClientePorId/ObterClientePorIdUseCase.cs
@@ -0,0 +1,21 @@
+﻿namespace GestaoDePedidos.Application.UseCases.Clientes.ObterClientePorId;
+
+public class ObterClientePorIdUseCase(IGestaoDePedidosDbContext context) : IObterClientePorIdUseCase
+{
+    public async Task<Response<ObterClientePorIdUseCaseModel>> ExecuteAsync(Guid id)
+    {
+        var cliente = await context.Clientes
+            .AsNoTracking()
+            .FirstOrDefaultAsync(c => c.Id == id);
+
+        if (cliente is null)
+            return new NotFoundResponse<ObterClientePorIdUseCaseModel>(ErrorMessages.NotFound<Cliente>());
+
+        return new OkResponse<ObterClientePorIdUseCaseModel>(new ObterClientePorIdUseCaseModel
+        {
+            Nome = cliente.Nome,
+            Email = cliente.Email,
+            Documento = cliente.Documento
+        });
+    }
+}

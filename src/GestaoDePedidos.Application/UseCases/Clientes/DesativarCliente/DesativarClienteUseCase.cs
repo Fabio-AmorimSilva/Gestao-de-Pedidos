58aac06d078d76123224e403a90e3e1100b02f82
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2026-05-19 23:27:26 -0300

    feat: Criar DesativarClienteUseCase e classes relacionadas

---
+++ src/GestaoDePedidos.Application/UseCases/Clientes/DesativarCliente/DesativarClienteUseCase.cs
@@ -0,0 +1,18 @@
+﻿namespace GestaoDePedidos.Application.UseCases.Clientes.DesativarCliente;
+
+public class DesativarClienteUseCase(IGestaoDePedidosDbContext context) : IDesativarClienteUseCase
+{
+    public async Task<Response> DesativarClienteAsync(Guid id)
+    {
+        var cliente = await context.Clientes
+            .AsNoTracking()
+            .FirstOrDefaultAsync(c => c.Id == id && c.Ativo);
+
+        if (cliente is null)
+            return new NotFoundResponse<Guid>(ErrorMessages.NotFound<Cliente>());
+
+        cliente.Desativar();
+
+        return new NoContentResponse();
+    }
+}
---
+++ src/GestaoDePedidos.Application/UseCases/Clientes/DesativarCliente/IDesativarClienteUseCase.cs
@@ -0,0 +1,6 @@
+﻿namespace GestaoDePedidos.Application.UseCases.Clientes.DesativarCliente;
+
+public interface IDesativarClienteUseCase
+{
+    Task<Response> DesativarClienteAsync(Guid id);
+}

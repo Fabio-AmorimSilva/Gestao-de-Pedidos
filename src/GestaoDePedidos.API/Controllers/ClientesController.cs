b98147cb0551d20fa7d4ea442ce3f9c74814a92c
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2026-05-20 20:16:13 -0300

    feat: Adicionando sufixo ao endereço do endpoints de desativar cliente

--- src/GestaoDePedidos.API/Controllers/ClientesController.cs
+++ src/GestaoDePedidos.API/Controllers/ClientesController.cs
@@ -11,7 +11,7 @@ public class ClientesController(UseCaseValidation validation) : BaseController(v
     public async Task<IActionResult> Get(Guid id)
         => await Execute<IObterClientePorIdUseCase, Guid, Response<ObterClientePorIdUseCaseModel>>(id);
     
-    [HttpPatch("{id:guid}")]
+    [HttpPatch("{id:guid}/deactivate")]
     public async Task<IActionResult> Deactivate(Guid id)
         => await Execute<IDesativarClienteUseCase, Guid, Response<Unit>>(id);
     

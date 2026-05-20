73be948a1a807a3fc26c8597d3b0048efd827774
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2026-05-19 23:20:48 -0300

    chore: Removing ClienteService.cs and IClienteService.cs

--- src/GestaoDePedidos.Application/Services/Clientes/IClienteService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GestaoDePedidos.Application.Services.Clientes;
-
-public interface IClienteService
-{
-    Task<Response<Guid>> CriarClienteAsync(Cliente dto);
-    Task<Response<ClienteResponseDto>> ObterClientePorIdAsync(Guid id);
-    Task<Response<IEnumerable<ClienteResponseDto>>> ObterClientesAsync();
-    Task<Response> DesativarClienteAsync(Guid id);
-}

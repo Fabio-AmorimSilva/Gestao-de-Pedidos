42fcc76fe75cb5b36dc003eae926cfb5bb951b68
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2026-05-19 23:25:56 -0300

    feat: Criando IDesativarProdutoUseCase e classes relacionadas

---
+++ src/GestaoDePedidos.Application/UseCases/Produtos/DesativarProduto/DesativarProdutoUseCase.cs
@@ -0,0 +1,18 @@
+﻿namespace GestaoDePedidos.Application.UseCases.Produtos.DesativarProduto;
+
+public class DesativarProdutoUseCase(IGestaoDePedidosDbContext context) : IDesativarProdutoUseCase
+{
+    public async Task<Response> ExecuteAsync(Guid id)
+    {
+        var produto = await context.Produtos
+            .FirstOrDefaultAsync(p => p.Id == id && p.Ativo);
+
+        if (produto is null)
+            return new NotFoundResponse<Guid>(ErrorMessages.NotFound<Produto>());
+
+        produto.Desativar();
+        await context.SaveChangesAsync();
+
+        return new NoContentResponse();
+    }
+}
---
+++ src/GestaoDePedidos.Application/UseCases/Produtos/DesativarProduto/IDesativarProdutoUseCase.cs
@@ -0,0 +1,6 @@
+﻿namespace GestaoDePedidos.Application.UseCases.Produtos.DesativarProduto;
+
+public interface IDesativarProdutoUseCase
+{
+    Task<Response> ExecuteAsync(Guid id);
+}

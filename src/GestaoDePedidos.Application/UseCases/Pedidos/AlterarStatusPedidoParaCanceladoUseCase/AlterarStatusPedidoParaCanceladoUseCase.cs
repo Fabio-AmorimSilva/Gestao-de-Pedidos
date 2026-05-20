e83fc1788231d0da268f043a22a37bdfd677ba4f
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2026-05-20 12:05:06 -0300

    feat: Adicionando estorno de estoque quando o pedido é cancelado

--- src/GestaoDePedidos.Application/UseCases/Pedidos/AlterarStatusPedidoParaCanceladoUseCase/AlterarStatusPedidoParaCanceladoUseCase.cs
+++ src/GestaoDePedidos.Application/UseCases/Pedidos/AlterarStatusPedidoParaCanceladoUseCase/AlterarStatusPedidoParaCanceladoUseCase.cs
@@ -9,9 +9,27 @@ public class AlterarStatusPedidoParaCanceladoUseCase(IGestaoDePedidosDbContext c
         if(pedido is null)
             return new NotFoundResponse<Pedido>(ErrorMessages.NotFound<Pedido>());
         
+        var produtosIds = context.Pedidos
+            .SelectMany(p => p.Itens)
+            .Select(produto => produto.ProdutoId);
+        
+        var produtosExists = await context.Produtos
+            .AnyAsync(p => produtosIds.Contains(p.Id));
+            
+        if(!produtosExists)
+            return new NotFoundResponse<Produto>(ErrorMessages.NotFound<Produto>());
+        
         var statusAnterior = pedido.Status;
         pedido.StatusCancelado();
+        
+        var itens = pedido.Itens;
 
+        foreach (var item in itens)
+        {
+            var produto = await context.Produtos.FirstAsync(p => p.Id == item.ProdutoId);
+            produto.AdicionarAoEstoque(item.Quantidade);
+        }
+        
         var historico = new PedidoHistoricoStatus(
             pedidoId: pedido.Id,
             statusAnterior: statusAnterior,

6ebdc8f2263bb9d0a858a9ecd0c709d2bca1f511
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2026-05-20 15:21:55 -0300

    feat: Criando conversor de retorno de datas de acordo com o fuso de São Paulo

---
+++ src/GestaoDePedidos.API/Utils/Converters/SaoPauloDateTimeConverter.cs
@@ -0,0 +1,42 @@
+﻿namespace GestaoDePedidos.API.Utils.Converters;
+
+public class SaoPauloDateTimeConverter : JsonConverter<DateTime>
+{
+    private static readonly TimeZoneInfo TimeZone =
+        TimeZoneInfo.FindSystemTimeZoneById(
+            RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+                ? "E. South America Standard Time"
+                : "America/Sao_Paulo"
+        );
+
+    public override DateTime Read(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options
+    )
+    {
+        return reader.GetDateTime();
+    }
+
+    public override void Write(
+        Utf8JsonWriter writer,
+        DateTime value,
+        JsonSerializerOptions options
+    )
+    {
+        var utcDate = value.Kind == DateTimeKind.Utc
+                ? value
+                : DateTime.SpecifyKind(
+                    value,
+                    DateTimeKind.Utc
+                );
+
+        var saoPauloDate =
+            TimeZoneInfo.ConvertTimeFromUtc(
+                utcDate,
+                TimeZone
+            );
+
+        writer.WriteStringValue(saoPauloDate.ToString("yyyy-MM-ddTHH:mm:ss"));
+    }
+}

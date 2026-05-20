2c6d3fc3a3f5e3f6fe35f773ed303ce11b78e7a6
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2026-05-19 23:32:12 -0300

    feat: Criando GlobaUsings para todas as camadas do projeto

---
+++ src/GestaoDePedidos.Infrastructure/GlobalUsings.cs
@@ -0,0 +1,8 @@
+global using System.Reflection;
+global using GestaoDePedidos.Application.Common.Interfaces;
+global using GestaoDePedidos.Infrastructure.Data;
+global using GestoDePedidos.Domain.Entities;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Diagnostics;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+global using Microsoft.Extensions.Configuration;
---
+++ src/GestoDePedidos.Domain/GlobalUsings.cs
@@ -0,0 +1,5 @@
+﻿global using Ardalis.Specification;
+global using CommunityToolkit.Diagnostics;
+global using GestoDePedidos.Domain.Common;
+global using GestoDePedidos.Domain.Entities;
+global using GestoDePedidos.Domain.Enums;

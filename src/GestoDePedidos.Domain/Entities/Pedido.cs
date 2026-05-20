56caee17e0642e5fdaef138a0ef2eec52bd7bca5
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2026-05-20 10:22:29 -0300

    feat: Adicionando métodos para mudanças de status do pedido

--- src/GestoDePedidos.Domain/Entities/Pedido.cs
+++ src/GestoDePedidos.Domain/Entities/Pedido.cs
@@ -6,8 +6,8 @@ public class Pedido : Entity
     public StatusPedido Status { get; private set; }
     public decimal Total { get; private set; }
     public DateTime DataCriacao { get; set; }
-    
-    private List<PedidoItem> _itens;
+
+    private List<PedidoItem> _itens = [];
     public IReadOnlyCollection<PedidoItem> Itens => _itens;
 
     public Pedido(
@@ -19,10 +19,34 @@ public class Pedido : Entity
         Total = TotalItens();
         DataCriacao = DateTime.UtcNow;
     }
-    
+
+    public void StatusPago()
+    {
+        if (Status is not StatusPedido.Criado)
+            throw new Exception($"Um pedido só pode ser pago caso esteja em status {StatusPedido.Criado}");
+
+        Status = StatusPedido.Pago;
+    }
+
+    public void StatusEnviado()
+    {
+        if (Status is not StatusPedido.Pago)
+            throw new Exception($"Um pedido só pode ser enviado caso esteja em status {StatusPedido.Pago}");
+
+        Status = StatusPedido.Enviado;
+    }
+
+    public void StatusCancelado()
+    {
+        if (Status is not StatusPedido.Criado)
+            throw new Exception($"Um pedido só pode ser cancelado caso esteja em status {StatusPedido.Criado}");
+
+        Status = StatusPedido.Cancelado;
+    }
+
     public void AddItem(PedidoItem item)
         => _itens.Add(item);
-    
+
     public decimal TotalItens()
         => _itens.Sum(pedidoItem => pedidoItem.Total);
-}  
+}

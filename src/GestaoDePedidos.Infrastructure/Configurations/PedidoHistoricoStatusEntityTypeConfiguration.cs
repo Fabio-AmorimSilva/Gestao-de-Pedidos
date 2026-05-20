e171cec652425b4549f2189631300c645d8fc637
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2026-05-20 09:59:02 -0300

    feat: Criação da configuração PedidoHistoricoStatusEntityTypeConfiguration.cs

---
+++ src/GestaoDePedidos.Infrastructure/Configurations/PedidoHistoricoStatusEntityTypeConfiguration.cs
@@ -0,0 +1,35 @@
+﻿namespace GestaoDePedidos.Infrastructure.Configurations;
+
+public class PedidoHistoricoStatusEntityTypeConfiguration : IEntityTypeConfiguration<PedidoHistoricoStatus>
+{
+    public void Configure(EntityTypeBuilder<PedidoHistoricoStatus> builder)
+    {
+        builder
+            .ToTable("PedidoHistoricosStatus");
+        
+        builder
+            .HasKey(phs => phs.Id);
+
+        builder
+            .Property(phs => phs.StatusAnterior)
+            .IsRequired();
+        
+        builder
+            .Property(phs => phs.StatusPosterior)
+            .IsRequired();
+
+        builder
+            .Property(phs => phs.DataAlteracao)
+            .IsRequired();
+
+        builder
+            .Property(phs => phs.Motivo)
+            .IsRequired(false);
+
+        builder
+            .HasOne<Pedido>(phs => phs.Pedido)
+            .WithMany()
+            .HasForeignKey(phs => phs.PedidoId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}

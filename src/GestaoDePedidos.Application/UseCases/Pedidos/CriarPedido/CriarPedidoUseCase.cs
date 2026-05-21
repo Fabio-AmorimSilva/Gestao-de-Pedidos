6a8c73b747d3cdb97ddc33d98133483160db92e0
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2026-05-21 09:50:18 -0300

    refactor: Adjustando validações para pedido com produto inativo e estoque insuficiente

--- src/GestaoDePedidos.Application/UseCases/Pedidos/CriarPedido/CriarPedidoUseCase.cs
+++ src/GestaoDePedidos.Application/UseCases/Pedidos/CriarPedido/CriarPedidoUseCase.cs
@@ -17,31 +17,36 @@ public class CriarPedidoUseCase(IGestaoDePedidosDbContext context) : ICriarPedid
 
         foreach (var item in model.Itens)
         {
-            var produto = await context.Produtos.FirstOrDefaultAsync(p =>
-                p.Id == item.ProdutoId &&
-                p.Estoque >= item.Quantidade &&
-                p.Ativo
-            );
+            var produto = await context.Produtos.FirstOrDefaultAsync(p => p.Id == item.ProdutoId);
             
             if(produto is null)
                 return new NotFoundResponse<Guid>(ErrorMessages.NaoEncontrado<Produto>());
-
+            
+            if(!produto.Ativo)
+                return new UnprocessableResponse<Guid>(ErrorMessages.ProdutoInativo());
+                
+            var isEstoqueValido = produto.RemoverDoEstoque(item.Quantidade);
+            
+            if(!isEstoqueValido)
+                return new UnprocessableResponse<Guid>(ErrorMessages.EstoqueInsuficiente());
+            
             var pedidoItem = new PedidoItem(
                 produtoId: produto.Id,
                 quantidade: item.Quantidade,
                 preco: produto.Preco
             );
             
-            var isEstoqueValido = produto.RemoverDoEstoque(item.Quantidade);
-            
-            if(!isEstoqueValido)
-                return new UnprocessableResponse<Guid>(ErrorMessages.EstoqueInsuficiente());
-            
             pedido.AddItem(pedidoItem);
             
             await context.Pedidos.AddAsync(pedido);
         }
         
+        var historico = new PedidoHistoricoPreco(
+            pedidoId: pedido.Id,
+            preco: pedido.TotalItens()
+        );
+        
+        await context.PedidoHistoricoPreco.AddAsync(historico);       
         await context.SaveChangesAsync();
 
         return new CreatedResponse<Guid>(pedido.Id);

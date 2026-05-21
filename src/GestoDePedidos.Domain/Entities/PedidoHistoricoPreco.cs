c623cd66f6a86d292648951e147d1873a5ab9c79
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2026-05-21 09:51:02 -0300

    feat: Criando nova entidade PedidoHistoricoPreco

---
+++ src/GestoDePedidos.Domain/Entities/PedidoHistoricoPreco.cs
@@ -0,0 +1,22 @@
+﻿namespace GestoDePedidos.Domain.Entities;
+
+public class PedidoHistoricoPreco : Entity, IAuditableEntity
+{
+    public Guid PedidoId { get; private set; }
+    public Pedido Pedido { get; private set; }
+    public decimal Preco { get; private set; }
+    
+    public DateTime DataCriacao { get; set; }
+    public DateTime? DataAtualizacao { get; set; }
+
+    public PedidoHistoricoPreco(
+        Guid pedidoId,
+        decimal preco
+    )
+    {
+        Guard.IsNotDefault(pedidoId);
+        
+        PedidoId = pedidoId;
+        Preco = preco;
+    }
+}
